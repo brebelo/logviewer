5fa4ca0b3f8276a7ec0bc3969c232d05fd691f71
brebelo <brandonrebelopro@gmai.com>
2026-08-07 19:23:04 +0200

    ajout finaux

--- testdoc.py
+++ testdoc.py
@@ -2,27 +2,41 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+fichier_csv = st.file_uploader("Merci d'ajouter votre fichier", type="csv")
 
-df = pd.read_csv('InfoLog54.csv', on_bad_lines="skip") # lis le .csv et skip les lign
-df.info() #debug sur les lignes/colonnes 
+if fichier_csv is not None:
+    df = pd.read_csv(fichier_csv, on_bad_lines='skip')
 
-def couleur(row):
-    couleur_cellule = 'background-color: yellow' if row["MsgText"] == "Cutting Unit - Cut startet" else ''
-    styles = [couleur_cellule] * len(row)
-    return styles
+else:
+    st.info("Merci d'ajouter un fichier .CSV (logs cutting)")
+    st.stop()
 
-def couleur(row):
-    couleur_cellule = 'background-color: yellow' if row["MsgText"] == "Cutting completed" else ''
-    styles = [couleur_cellule] * len(row)
-    return styles
+st.title("Lecture des cycles de coupes - Cutting Machine (A mettre en plein ecran)")
+
+#df = pd.read_csv('InfoLog24.csv', on_bad_lines="skip") # lis le .csv et skip les lign
+
+def colorier_lignes(row):
+    if row["MsgText"] == "Cutting Unit - Cut startet":
+        return ['color: #00635D; background-color: #01172F'] * len(row)
+    elif row["MsgText"] == "Cutting completed":
+        return ['color: #4C212A; background-color: lightgrey'] * len(row)  # ou yellow aussi
+    elif row["MsgText"] == "Cutting Unit - Cut finished":
+        return ['color: #4C212A; background-color: lightgrey'] * len(row)
+    else:
+        return [''] * len(row)
 
 #df_reindexed = df.reindex(columns='Test')
 #convertir le temps pm en am 
-#
-df = df.drop(columns=['Time_ms', 'MsgProc', 'StateAfter','MsgClass','Var1','Var2','Var3','Var4','Var5','Var6','Var7','Var8','PLC'])
 
 
-df = df.style.apply(couleur, axis=1)
+df['Numero de Layer'] = df['MsgText'].str.extract(r'Layer .*?(\d+)')
+df['Numero de Palette'] = df['MsgText'].str.extract(r'Pallet Nr:\s*(\d+)')
+
+#a = []
+df = df.drop(columns=['Time_ms', 'MsgProc', 'StateAfter','MsgClass','Var1','Var2','Var3','Var4','Var5','Var6','Var7','Var8','MsgNumber','PLC'])
+#df = df.insert(loc=4, column='Layer', value=a)
+
+df = df.style.apply(colorier_lignes, axis=1)
 
 
 #st.dataframe(df_reindexed) # print de la dataframe df
