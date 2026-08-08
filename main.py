a8113e7a61adc10052b7d2175bc86ee9083349d8
REBELO <brandon.rebelo@siemens-energy.com>
2026-08-08 11:01:26 +0200

    Structure et mise en forme du main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+##################################################################################################################################################
+#                                                                                                                                                #
+#                                                           Author : Brandon Rebelo                                                              #
+#                                                                                                                                                #
+##################################################################################################################################################
+
 import pandas as pd
-import numpy as np
 import streamlit as st
 
-
+# Gestion du fichier csv, puis importation comme reference dans df
 fichier_csv = st.file_uploader("Merci d'ajouter votre fichier", type="csv")
-
 if fichier_csv is not None:
     df = pd.read_csv(fichier_csv, on_bad_lines='skip')
 
@@ -12,10 +16,14 @@ else:
     st.info("Merci d'ajouter un fichier .CSV (logs cutting)")
     st.stop()
 
+# Titre
 st.title("Lecture des cycles de coupes - Cutting Machine (A mettre en plein ecran)")
 
-#df = pd.read_csv('InfoLog24.csv', on_bad_lines="skip") # lis le .csv et skip les lign
+# Structure et mise en forme des colonnes pas necessaires
+df = df.drop(columns=['Time_ms', 'MsgProc', 'StateAfter','MsgClass','Var1','Var2','Var3','Var4','Var5','Var6','Var7','Var8','MsgNumber','PLC'])
 
+
+# Colores les lignes souhaitées
 def colorier_lignes(row):
     if row["MsgText"] == "Cutting Unit - Cut startet":
         return ['color: #00635D; background-color: #01172F'] * len(row)
@@ -26,22 +34,16 @@ def colorier_lignes(row):
     else:
         return [''] * len(row)
 
-#df_reindexed = df.reindex(columns='Test')
-#convertir le temps pm en am 
-
-
+# Rajout de la colone Layer et Palett voulu
 df['Numero de Layer'] = df['MsgText'].str.extract(r'Layer .*?(\d+)')
 df['Numero de Palette'] = df['MsgText'].str.extract(r'Pallet Nr:\s*(\d+)')
-
-#a = []
-df = df.drop(columns=['Time_ms', 'MsgProc', 'StateAfter','MsgClass','Var1','Var2','Var3','Var4','Var5','Var6','Var7','Var8','MsgNumber','PLC'])
-#df = df.insert(loc=4, column='Layer', value=a)
 
 df = df.style.apply(colorier_lignes, axis=1)
 
 
-#st.dataframe(df_reindexed) # print de la dataframe df
-st.dataframe(df) #print de la dataframe avec couleur, test
+
+# Print de la dataframe finale
+st.dataframe(df) 
 
 
 
